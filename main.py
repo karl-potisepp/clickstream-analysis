3720cf90e9ab9700c1a0e568aa748f0d5269ee90
dragonath <dragonath@80df0692-b355-11de-8af0-9d1a786950a5>
2009-10-08 11:09:23 +0000

    issue #1 & some todos

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
 import apachelogs
 from datetime import datetime, date, time
 import uuid
 import numpy as np
 
-def filter(line):
+#function to evaluate whether a log file line is useful or not
+def filter(line, ip_blacklist):
 	result = False
+	#if ip is in blacklist
+	if ip_blacklist.count(line.ip) > 0:
+		return False
+	#no images, CSS or JS files
 	result = result or line.url.find(".png") != -1
 	result = result or line.url.find(".gif") != -1
 	result = result or line.url.find(".jpg") != -1
 	result = result or line.url.find(".png") != -1
 	result = result or line.url.find(".js") != -1
 	result = result or line.url.find(".css") != -1
+	result = result or line.url.find(".ico") != -1
+
+	#no HTTP OPTIONS
+	result = result or line.http_method.find("OPTIONS") != -1
+
+	#no home directories
+	result = result or line.http_method.find("/~") != -1
+
 	return result
 
 if __name__ == "__main__":
@@ -22,13 +38,25 @@ if __name__ == "__main__":
 	sessions = {}
 
 	last_ip = {}
+
+	#list of IPs deemed non-useful (automated pollers, search robots)
+	ip_blacklist = []
 	
 	urls = set([])
 	count = 0
 
 	for line in log:
-		if filter(line):
+
+		#whether or not the requested file is robots.txt
+		#if it is, we add the ip to the blacklist
+		if line.url.find("robots.txt") != -1:
+			ip_blacklist.append(line.ip)
+
+		if filter(line, ip_blacklist):
 			continue
+
+		#TODO: strip parts containing ?var=id&var2=id2 from the end of URLs 
+
 		urls.add(line.url)
 		line.time = line.time.split()[0]
 		line.date = datetime.strptime(line.time, '%d/%b/%Y:%H:%M:%S')
@@ -50,8 +78,9 @@ if __name__ == "__main__":
 		count +=1
 
 
-
     #sessions dict contains all sessions
+
+	#TODO: output a list of sessions
     
 
 	print "Matching lines: ", count
